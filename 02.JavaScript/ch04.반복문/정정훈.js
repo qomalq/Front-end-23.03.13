1a1796b41a2dc627baa4c750aa4978e97f8dfc9e
jeong jeong hoon <rmsdnr45@naver.com>
2023-03-19 18:19:32 +0900

    ads

--- 02.JavaScript/ch04.반복문/정정훈.js
+++ 02.JavaScript/ch04.반복문/정정훈.js
@@ -8,35 +8,31 @@ console.log(` ${N} * ${i} = ${N*i}`)
 // // 알람 시계
 let H = 10;
 let M = 10;
-let Hour
-let Min
-if(M >= 45){
-    Min == M - 45;
-}else{
-    Hour == H - 1;
-    Min == M - 45 + 60; 
-    if(H == 0) {
-        Hour == 24;
+
+if(H !== 0) {
+    if(M >= 45) {
+        console.log(`${H}시 ${M - 45}분`);
     }else{
-        
+        console.log(`${H - 1}시 ${60 - (45 - M)}분`);
     }
+}else if(H === 0 && M < 45) {
+    console.log(`23 ${60 - (45 - M)}`);
+}else if(H === 0 && M >= 45) {
+    console.log(`${H}시 ${M - 45}분`);
 }
-console.log(`${Hour}시 ${Min}`)
 
-
-// console.log(`성적: ${score}, 학점: ${grade}`);
-
-// //주사위
+// // //주사위
 let dice1 = parseInt(Math.ceil(Math.random() * 6));
 
 let dice2 = parseInt(Math.ceil(Math.random() * 6));
 
 let dice3 = parseInt(Math.ceil(Math.random() * 6));
 
-console.log(dice1, dice2, dice3)
-if (dice1 == dice2 && dice1 == dice3)
+if (dice1 == dice2 && dice1 == dice3) {
+    console.log(10000 + dice1 * 1000);
+} else if (dice1 === (dice2 || dice3) || dice2 === dice3) {
+    console.log(dice1 === dice2 ? 1000 + dic1 * 100 : 1000 + dice2 * 100);
+  } else {
+    console.log(Math.max(dice1,dice2,dice3)*100);
+  }
   
-    if (dice1 == dice2 || dice2 != dice3 && dice1 != dice3)
-    
-
-console.log(``)
